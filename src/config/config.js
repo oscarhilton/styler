c8fd109beffcf0df04432acb6c4e9473652f04fc
Oscar Hilton <oscar.hilton@mobile-5.com>
2019-06-18 18:28:20 +0100

    Remove congfig

--- src/config/config.js
+++ src/config/config.js
@@ -5,31 +5,31 @@ let talentApiUrl;
 
 switch (DEPLOY_ENV) {
   case "dev":
-    apiUrl = "https://dev-admin-skybet.mobile-5.com/api/";
-    mediaUrl = "https://dev-admin-skybet.mobile-5.com";
-    talentApiUrl = "https://dev-api-skybet.mobile-5.com/api/";
-    pdfUrl = "https://dev-api-skybet.mobile-5.com/";
+    apiUrl = "";
+    mediaUrl = "";
+    talentApiUrl = "";
+    pdfUrl = "";
     break;
 
   case "qa":
-    apiUrl = "https://qa-admin-skybet.mobile-5.com/api/";
-    mediaUrl = "https://qa-admin-skybet.mobile-5.com";
-    talentApiUrl = "https://dev-api-skybet.mobile-5.com/api/";
-    pdfUrl = "https://dev-api-skybet.mobile-5.com/";
+    apiUrl = "";
+    mediaUrl = "";
+    talentApiUrl = "";
+    pdfUrl = "";
     break;
 
   case "stage":
-    apiUrl = "https://stg-admin-skybet.mobile-5.com/api/";
-    mediaUrl = "https://stg-admin-skybet.mobile-5.com";
-    talentApiUrl = "https://stg-api-skybet.mobile-5.com/api/";
-    pdfUrl = "https://stg-api-skybet.mobile-5.com/";
+    apiUrl = "";
+    mediaUrl = "";
+    talentApiUrl = "";
+    pdfUrl = "";
     break;
 
   default:
-    apiUrl = "https://stg-admin-skybet.mobile-5.com/api/";
-    mediaUrl = "https://stg-admin-skybet.mobile-5.com";
-    talentApiUrl = "https://stg-api-skybet.mobile-5.com/api/";
-    pdfUrl = "https://stg-api-skybet.mobile-5.com/";
+    apiUrl = "";
+    mediaUrl = "";
+    talentApiUrl = "";
+    pdfUrl = "";
 };
 
 export default {
